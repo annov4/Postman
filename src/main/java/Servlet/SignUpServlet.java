6b96b0ae6e42c012b70b35185a08a99bda2f1e61
annov4 <137298843+annov4@users.noreply.github.com>
2024-04-01 03:01:41 +0300

    third commit

--- src/main/java/Servlet/SignUpServlet.java
+++ src/main/java/Servlet/SignUpServlet.java
@@ -21,11 +21,6 @@ public class SignUpServlet extends HttpServlet {
         String login = request.getParameter("login");
         String password = request.getParameter("password");
 
-        if (login == null || password == null) { // проверяем наличие логина и пароля
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return;
-        }
-
         accountService.addNewUser(new User(login, password)); // добавляем в аккаунт сервис
 
         response.setStatus(HttpServletResponse.SC_OK); // возвращаем статус
